d9c8adc00d7c6340dcbb20987bb0569e6f452bf1
seans <sean.stuckless@gmail.com>
2016-01-01 09:26:09 -0500

    Updated the generation script to process each xml and url separately to better collect errors.

--- buildpluginsxml.sh
+++ buildpluginsxml.sh
@@ -4,11 +4,26 @@ PLUGINFILE=SageTVPluginsV9.xml
 PLUGINMD5=SageTVPluginsV9.md5
 TMPFILE=SageTVPluginsV9TMP.xml
 DATE=`date +%Y.%0m%0d.%0H%0M`
+FAILED=""
 
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" > $TMPFILE
 echo "<PluginRepository version=\"${DATE}\">" >> $TMPFILE
 # finding regular xml manifests
-cat `find plugins/ -iname '*.xml' -exec echo {} \;` | grep -v '<?xml ' >> $TMPFILE
+#cat `find plugins/ -iname '*.xml' -exec echo {} \;` | grep -v '<?xml ' >> $TMPFILE
+
+for file in `find plugins/ -name '*.xml'` ; do
+    # validate the xml we just got
+    xmllint --noout $file
+    if [ $? -eq 0 ]
+    then
+      echo "Adding Plugin $file"
+      cat $file | grep -v '<?xml ' >> $TMPFILE
+    else
+      FAILED="${FAILED} ${file}"
+      echo "XML File for ${file} is not valid.  Skipping..." >&2
+    fi
+done
+
 
 # resolve .url locators
 mkdir tmp
@@ -22,13 +37,23 @@ for file in `find ../plugins/ -name '*.url'` ; do
     wget -O tmp.xml "$url"
     if [ $? -ne 0 ]
     then
-        echo "Failed to resolve $url.  Skipping..."
+        FAILED="${FAILED} ${file}"
+        echo "Failed to resolve $url in ${file}.  Skipping..."
     else
-        echo "Adding $URL"
-        cat tmp.xml | grep -v '<?xml ' >> ../$TMPFILE
+        # validate the xml we just got
+        xmllint --noout tmp.xml
+        if [ $? -eq 0 ]
+        then
+          echo "Adding Plugin from $url"
+          cat tmp.xml | grep -v '<?xml ' >> ../$TMPFILE
+        else
+          FAILED="${FAILED} ${file}"
+          echo "XML File for ${url} is not valid.  Skipping ${file} ..." >&2
+        fi
     fi
 done
 cd ..
+rm -rf tmp
 
 echo '</PluginRepository>' >> $TMPFILE
 
@@ -38,7 +63,28 @@ then
   rm $PLUGINFILE
   mv $TMPFILE $PLUGINFILE
   md5sum $PLUGINFILE | tr " " "\n" | grep -v 'xml' > $PLUGINMD5
-  echo "Successfully created ${PLUGINFILE} and ${PLUGINMD5}"
+  echo ""
+  echo ""
+
+  if [ "$FAILED" != "" ] ; then
+
+    echo ""
+    echo ""
+    echo "Successfully created ${PLUGINFILE} and ${PLUGINMD5} BUT Some plugins are omitted"
+    echo "** PLUGIN FAILURES **"
+    echo "$FAILED"
+    echo "** PLUGIN FAILURES **"
+  else
+    echo "Successfully created ${PLUGINFILE} and ${PLUGINMD5}"
+  fi
 else
+  echo ""
+  echo ""
   echo "XML File is not valid" >&2
+  if [ "$FAILED" != "" ] ; then
+    echo "** PLUGIN FAILURES **"
+    echo "$FAILED"
+    echo "** PLUGIN FAILURES **"
+  fi
 fi
+
