08fd498887c93bbc5907174a07f8876b8ba87e70
seans <sean.stuckless@gmail.com>
2015-12-31 19:07:54 -0500

    added support for resolving manifests in remote locations

--- README.md
+++ README.md
@@ -10,6 +10,8 @@ You can add more than one xml in your folder, but, the start and end of each xml
 
 ie, the plugin xml is exactly the same as your Plugin's Manifest.
 
+If you don't want to host your manifest file in this repo, you can create a plugin entry in the `plugins` directory, and create one or more `.url` files that contain a single line that resolves to your plugin manifest xml.  In this way, you can keep your manifest file in your own repository, and simply link to it from here.
+
 When you commit your changes, create a `PULL REQUEST` and your plugin manifest will be included (at some timed interval, usually once a day, or sooner)
 
 Do not edit the `SageTVPluginsV9.xml` file.  This file is automatically re-generated based on the content in the `plugins` directory.
--- buildpluginsxml.sh
+++ buildpluginsxml.sh
@@ -7,7 +7,29 @@ DATE=`date +%Y.%0m%0d.%0H%0M`
 
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" > $TMPFILE
 echo "<PluginRepository version=\"${DATE}\">" >> $TMPFILE
-cat `find plugins/ -iname '*.xml' -exec echo {} \;` >> $TMPFILE
+# finding regular xml manifests
+cat `find plugins/ -iname '*.xml' -exec echo {} \;` | grep -v '<?xml ' >> $TMPFILE
+
+# resolve .url locators
+mkdir tmp
+cd tmp
+for file in `find ../plugins/ -name '*.url'` ; do
+    if [ -e tmp.xml ] ; then
+       rm tmp.xml
+    fi
+    url=`cat $file | head -n1`
+    echo "Resolving Reference URL $url"
+    wget -O tmp.xml "$url"
+    if [ $? -ne 0 ]
+    then
+        echo "Failed to resolve $url.  Skipping..."
+    else
+        echo "Adding $URL"
+        cat tmp.xml | grep -v '<?xml ' >> ../$TMPFILE
+    fi
+done
+cd ..
+
 echo '</PluginRepository>' >> $TMPFILE
 
 xmllint --noout $TMPFILE
